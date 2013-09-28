55a8343aec6bd0cc0b4a068db28da36b2637f34c
Razzi <razzi53@gmail.com>
2013-09-28 17:33:20 -0400

    add os

--- outpatient/outpatient/settings/base.py
+++ outpatient/outpatient/settings/base.py
@@ -3,6 +3,7 @@
 
 from os.path import abspath, basename, dirname, join, normpath
 from sys import path
+import os
 
 ########## PATH CONFIGURATION
 # Absolute filesystem path to the Django project directory:
