2b93813c2c4d669dbfa3170f02b178de829050e7
Dan Getelman <dgetelman@gmail.com>
2013-09-28 17:39:26 -0400

    Things change. That's the way it is.

--- outpatient/app/management/commands/send_messages.py
+++ outpatient/app/management/commands/send_messages.py
@@ -11,16 +11,14 @@ class Command(NoArgsCommand):
         to_send = to_send.filter(sent24=False)
 
         for reminder in to_send:
-            if reminder.sent24=False:
-                account_sid = ''
-                auth_token = ''
-
-                client = TwilioRestClient(account_sid, auth_token)
+            if reminder.sent24==False:
+                client = TwilioRestClient()
 
                 message = client.sms.messages.create(
                         body = "Dr Reminder 4 tomorrow: %s" % reminder.msg,
-                        to=recipient,
-                        from="+12156004771")
+                        to=reminder.recipient, from_="+12156004771")
+                reminder.sent24 = True
+                reminder.save()
                 print message.sid
 
         
